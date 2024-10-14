6c9de10b942f25b66ed15e196585ee23b8e2758f
Erin Millard <contact@erin.id.au>
2024-10-15 08:57:46 +1000

    WIP

--- src/token-authorizer.ts
+++ src/token-authorizer.ts
@@ -13,15 +13,21 @@ export type TokenAuthorizer = {
 export function createTokenAuthorizer(
   config: ProviderPermissionsConfig,
 ): TokenAuthorizer {
+  const repoResourcePatterns: Record<number, Pattern[]> = {};
   const repoConsumerPatterns: Record<number, Pattern[]> = {};
 
   for (let n = 1; n <= config.rules.repositories.length; ++n) {
+    const resourcePatterns: Pattern[] = [];
     const consumerPatterns: Pattern[] = [];
 
+    for (const resource of config.rules.repositories[n - 1].resources) {
+      resourcePatterns.push(createPattern(resource));
+    }
     for (const consumer of config.rules.repositories[n - 1].consumers) {
       consumerPatterns.push(createPattern(consumer));
     }
 
+    repoResourcePatterns[n] = resourcePatterns;
     repoConsumerPatterns[n] = consumerPatterns;
   }
 
@@ -30,7 +36,13 @@ export function createTokenAuthorizer(
       const consumer = `${consumerOwner}/${consumerRepo}`;
 
       for (let n = 1; n <= config.rules.repositories.length; ++n) {
-        if (anyPatternMatches(repoConsumerPatterns[n], consumer)) {
+        if (!anyPatternMatches(repoConsumerPatterns[n], consumer)) continue;
+
+        for (const repo of request.repositories) {
+          const resource = `${request.owner}/${repo}`;
+
+          if (!anyPatternMatches(repoResourcePatterns[n], resource)) continue;
+
           return [true, undefined];
         }
       }
--- test/suite/unit/authorize-token.spec.ts
+++ test/suite/unit/authorize-token.spec.ts
@@ -78,3 +78,34 @@ it("doesn't allow tokens for unauthorized consumer repositories", () => {
     }),
   ).toEqual([false, { type: "NO_MATCHING_REPOSITORY_RULE" }]);
 });
+
+it("doesn't allow tokens for undefined resource repositories", () => {
+  const authorizer = createTokenAuthorizer({
+    rules: {
+      repositories: [
+        {
+          resources: ["owner-a/repo-a", "owner-a/repo-b"],
+          consumers: ["owner-x/repo-x"],
+          permissions: { contents: "write", metadata: "read" },
+        },
+      ],
+    },
+  });
+
+  expect(
+    authorizer.authorizeForRepository("owner-x", "repo-x", {
+      role: undefined,
+      owner: "owner-a",
+      repositories: ["repo-y"],
+      permissions: { contents: "write" },
+    }),
+  ).toEqual([false, { type: "NO_MATCHING_REPOSITORY_RULE" }]);
+  expect(
+    authorizer.authorizeForRepository("owner-x", "repo-x", {
+      role: undefined,
+      owner: "owner-y",
+      repositories: ["repo-a"],
+      permissions: { contents: "write" },
+    }),
+  ).toEqual([false, { type: "NO_MATCHING_REPOSITORY_RULE" }]);
+});
