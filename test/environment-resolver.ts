cea5d2fd78ffd3652ae17819bbc84b9cf45709d1
Erin Millard <contact@erin.id.au>
2025-05-27 17:06:37 +1000

    Add test environment resolver

---
+++ test/environment-resolver.ts
@@ -0,0 +1,30 @@
+import type { EnvironmentResolver } from "../src/environment-resolver.js";
+import {
+  repoRefToString,
+  type RepoReference,
+} from "../src/github-reference.js";
+import { anyPatternMatches } from "../src/pattern.js";
+
+export type TestEnvironmentResolver = EnvironmentResolver & {
+  registerEnvironments: (repo: RepoReference, environments: string[]) => void;
+};
+
+export function createTestEnvironmentResolver(): TestEnvironmentResolver {
+  const envsByRepo: Record<string, string[]> = {};
+
+  return {
+    registerEnvironments(repo, environments) {
+      const repoName = repoRefToString(repo);
+
+      envsByRepo[repoName] ??= [];
+      envsByRepo[repoName].push(...environments);
+    },
+
+    async resolveEnvironments(repo, patterns) {
+      const repoName = repoRefToString(repo);
+      const repoEnvs = envsByRepo[repoName] ?? [];
+
+      return repoEnvs.filter((env) => anyPatternMatches(patterns, env));
+    },
+  };
+}
