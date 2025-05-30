f69c6a5ff9c1522958bc93d8385bbce6e4cbf429
Erin Millard <contact@erin.id.au>
2025-05-30 12:00:51 +1000

    WIP

--- script/build.js
+++ script/build.js
@@ -1,6 +1,4 @@
 import { build } from "esbuild";
-import { readFile } from "node:fs/promises";
-import { extname } from "node:path";
 
 const [, , outfile] = process.argv;
 
@@ -9,13 +7,6 @@ if (!outfile) {
   process.exit(1);
 }
 
-const addRequire = `// add require()
-const require = await (async () => {
-	const { createRequire } = await import("node:module");
-
-	return createRequire(import.meta.url);
-})();`;
-
 await build({
   entryPoints: ["src/main.ts"],
   bundle: true,
@@ -23,26 +14,6 @@ await build({
   sourcemap: true,
   platform: "node",
   target: "node20",
-  format: "esm",
+  format: "cjs",
   outfile,
-  banner: {
-    js: addRequire,
-  },
-  plugins: [
-    {
-      name: "dirname",
-      setup: (build) => {
-        build.onLoad({ filter: /.*/ }, async ({ path: fn }) => {
-          if (extname(fn) !== ".js") return undefined;
-
-          const original = await readFile(fn, "utf8");
-          const contents = original
-            .replaceAll("__dirname", "import.meta.dirname")
-            .replaceAll("__filename", "import.meta.filename");
-
-          return { contents };
-        });
-      },
-    },
-  ],
 });
