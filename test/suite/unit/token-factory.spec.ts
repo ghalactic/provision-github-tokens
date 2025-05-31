8562cfcdfcb8273586e177fd30f8d21fcd2e7332
Erin Millard <contact@erin.id.au>
2025-06-01 07:05:40 +1000

    WIP

---
+++ test/suite/unit/token-factory.spec.ts
@@ -0,0 +1,134 @@
+import { expect, it, vi } from "vitest";
+import {
+  createAppRegistry,
+  type AppRegistration,
+  type InstallationRegistration,
+} from "../../../src/app-registry.js";
+import { createFindIssuerOctokit } from "../../../src/issuer-octokit.js";
+import { createOctokitFactory } from "../../../src/octokit.js";
+import { createTokenFactory } from "../../../src/token-factory.js";
+import type { AppInput } from "../../../src/type/input.js";
+import type { TokenAuthResult } from "../../../src/type/token-auth-result.js";
+import {
+  createTestApp,
+  createTestInstallation,
+  createTestInstallationAccount,
+  createTestInstallationRepo,
+} from "../../github-api.js";
+
+vi.mock("@actions/core");
+vi.mock("@octokit/action");
+
+it("creates tokens based on token auth results", async () => {
+  const octokitFactory = createOctokitFactory();
+
+  const accountA = createTestInstallationAccount(
+    "Organization",
+    100,
+    "account-a",
+  );
+  const repoA = createTestInstallationRepo(accountA, "repo-a");
+  const appA = createTestApp(110, "app-a", "App A", { metadata: "read" });
+  const appRegA: AppRegistration = {
+    app: appA,
+    issuer: { enabled: true, roles: [] },
+    provisioner: { enabled: false },
+  };
+  const appAInstallationA = createTestInstallation(111, appA, accountA, "all");
+  const appAInstallationRegA: InstallationRegistration = {
+    installation: appAInstallationA,
+    repos: [repoA],
+  };
+  const appRegistry = createAppRegistry();
+  appRegistry.registerApp(appRegA);
+  appRegistry.registerInstallation(appAInstallationRegA);
+
+  const appsInput: AppInput[] = [
+    {
+      appId: 110,
+      privateKey: "<private key A>",
+      issuer: { enabled: true, roles: [] },
+      provisioner: { enabled: false },
+    },
+  ];
+  const findIssuerOctokit = createFindIssuerOctokit(
+    octokitFactory,
+    appRegistry,
+    appsInput,
+  );
+
+  const createTokens = createTokenFactory(findIssuerOctokit);
+
+  const notAllowedResult: TokenAuthResult = {
+    type: "ALL_REPOS",
+    request: {
+      consumer: { account: "account-a" },
+      tokenDec: {
+        shared: false,
+        as: undefined,
+        account: "account-a",
+        repos: [],
+        permissions: { metadata: "read" },
+      },
+      repos: [],
+    },
+    maxWant: "write",
+    have: { metadata: "read" },
+    isSufficient: false,
+    isMissingRole: false,
+    isAllowed: false,
+    rules: [],
+  };
+  const noIssuerResult: TokenAuthResult = {
+    type: "ALL_REPOS",
+    request: {
+      consumer: { account: "account-a" },
+      tokenDec: {
+        shared: false,
+        as: undefined,
+        account: "account-b",
+        repos: [],
+        permissions: { metadata: "read" },
+      },
+      repos: [],
+    },
+    maxWant: "read",
+    have: { metadata: "read" },
+    isSufficient: true,
+    isMissingRole: false,
+    isAllowed: true,
+    rules: [],
+  };
+  const createdResult: TokenAuthResult = {
+    type: "ALL_REPOS",
+    request: {
+      consumer: { account: "account-a" },
+      tokenDec: {
+        shared: false,
+        as: undefined,
+        account: "account-a",
+        repos: [],
+        permissions: { metadata: "read" },
+      },
+      repos: [],
+    },
+    maxWant: "read",
+    have: { metadata: "read" },
+    isSufficient: true,
+    isMissingRole: false,
+    isAllowed: true,
+    rules: [],
+  };
+
+  expect(
+    Array.from(
+      (
+        await createTokens([notAllowedResult, noIssuerResult, createdResult])
+      ).entries(),
+    ),
+  ).toEqual([
+    [notAllowedResult, { type: "NOT_ALLOWED" }],
+    [noIssuerResult, { type: "NO_ISSUER" }],
+    [createdResult, { type: "CREATED", token: {} }],
+  ]);
+});
