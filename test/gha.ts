96e238781eab1f2affdc6d8f037173024ad5f615
Erin Millard-Wright <contact@erin.id.au>
2026-04-26 10:43:54 +1000

    Make dog-fooding runs distinguishable

--- test/gha.ts
+++ test/gha.ts
@@ -1,26 +1,43 @@
 import { createTestOctokit, type TestOctokit } from "./octokit.js";
 
 export type GitHubActionsContext = {
+  eventName: string;
+  headRef: string;
   octokit: TestOctokit;
   owner: string;
-  repo: string;
   ref: string;
-  sha: string;
-  runId: string;
+  refName: string;
+  repo: string;
   runAttempt: string;
+  runId: string;
+  sha: string;
 };
 
 export function getGHAContext(): GitHubActionsContext {
   const {
+    GITHUB_EVENT_NAME: eventName = "",
+    GITHUB_HEAD_REF: headRef = "",
+    GITHUB_REF_NAME: refName = "",
     GITHUB_REF: ref = "",
     GITHUB_REPOSITORY: slug = "",
-    GITHUB_SHA: sha = "",
     GITHUB_RUN_ID: runId = "",
+    GITHUB_SHA: sha = "",
     GITHUB_RUN_ATTEMPT: runAttempt = "",
   } = process.env;
 
   const [owner, repo] = slug.split("/");
   const octokit = createTestOctokit();
 
-  return { octokit, owner, repo, ref, sha, runId, runAttempt };
+  return {
+    eventName,
+    headRef,
+    octokit,
+    owner,
+    ref,
+    refName,
+    repo,
+    runAttempt,
+    runId,
+    sha,
+  };
 }
--- test/suite/e2e/run-self.spec.ts
+++ test/suite/e2e/run-self.spec.ts
@@ -11,11 +11,23 @@ const ghaContext = getGHAContext();
 it(
   "can run itself via workflow_dispatch",
   async ({ onTestFinished }) => {
-    const run = await createSelfRunWorkflow(
-      onTestFinished,
-      ghaContext,
-      "example-label",
-    );
+    const { headRef, refName } = ghaContext;
+    const [prNumber] = refName.split("/");
+
+    const eventName = (() => {
+      if (ghaContext.eventName === "pull_request") return "pr";
+      return ghaContext.eventName.replace(/[^a-z]+/g, "-");
+    })();
+
+    const label = (() => {
+      if (!headRef) return `${eventName}-${refName.replace(/\//g, "-")}`;
+      if (!prNumber.match(/^[1-9][0-9]*$/)) {
+        return `${eventName}-${headRef.replace(/\//g, "-")}`;
+      }
+      return `${eventName}-${prNumber}-${headRef.replace(/\//g, "-")}`;
+    })();
+
+    const run = await createSelfRunWorkflow(onTestFinished, ghaContext, label);
 
     await expect(
       waitForWorkflowRunToSucceed(ghaContext, run),
