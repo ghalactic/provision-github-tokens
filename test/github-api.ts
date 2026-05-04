912673409d5b23a290aec9569ef27aab2d622096
Erin Millard-Wright <contact@erin.id.au>
2026-05-04 15:19:01 +1000

    Avoid extra helper calls

--- test/github-api.ts
+++ test/github-api.ts
@@ -114,7 +114,7 @@ export function createTestInstallationAccounts(
 
     return [
       account,
-      createTestInstallationRepos(account, ...(repoNames ?? [])),
+      repoNames ? createTestInstallationRepos(account, ...repoNames) : [],
     ];
   });
 }
